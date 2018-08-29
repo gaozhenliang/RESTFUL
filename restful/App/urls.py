a4321eefce4da8b7f3ed257b45319e6bbf4bb2a0
gaozhenliang <1336188490@qq.com>
2018-08-29 08:33:15 +0800

    使用restful给url添加可选的后缀

--- restful/App/urls.py
+++ restful/App/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from .views import views
+#格式后缀
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     #GET /students
@@ -10,3 +12,7 @@ urlpatterns = [
     #DELETE /students/id
     url(r'^students/(?P<pk>\d+)/$', views.studentDetail),
 ]
+
+#给路由添加后缀(添加api的后缀),  当添加此函数时，会把url按点号切割，如果是json则返回json格式的数据，
+#如果是api，则会把json格式的数据传递给模板进行渲染，再返回给客户端，这些操作都是这个函数去帮我们做的
+urlpatterns = format_suffix_patterns(urlpatterns)
--- restful/App/views/views.py
+++ restful/App/views/views.py
@@ -1,11 +1,6 @@
 from django.contrib.auth.models import User
-from django.http import JsonResponse
-from django.shortcuts import render,HttpResponse
 from App.models import Student,Grade
 
-# from rest_framework.parsers import JSONParser
-# from rest_framework.renderers import JSONRenderer
-# from django.utils.six import BytesIO
 
 from App.serializers import StudentSerializer
 
@@ -18,18 +13,14 @@ from rest_framework.response import Response
 
 
 @api_view(['GET','POST'])
-def studentsList(request):
+def studentsList(request,format=None):
     if request.method == 'GET':
         stus = Student.objects.all()
 
         serializer = StudentSerializer(stus,many=True)
-        #不需要制定json格式，返回客户端可以返回json或者HTML，返回HTML内容的话，会在浏览器中经过渲染成页面
         return Response(serializer.data,status=status.HTTP_200_OK)
 
     elif request.method == 'POST':
-        # content = JSONRenderer().render(request.POST)
-        # stream = BytesIO(content)
-        # stuDict = JSONParser().parse(stream)
         serializer = StudentSerializer(data=request.data)
 
         if serializer.is_valid():
@@ -39,7 +30,7 @@ def studentsList(request):
 
 
 @api_view(['GET','PUT','DELETE'])
-def studentDetail(request,pk):
+def studentDetail(request,pk,format=None):
     try:
         stu = Student.objects.get(pk=pk)
     except Student.DoesNotExist as e:
@@ -50,12 +41,8 @@ def studentDetail(request,pk):
         return Response(serializer.data)
 
     elif request.method == 'PUT':
-        # content = JSONRenderer().render(request.POST)
-        # stream = BytesIO(content)
-        # stuDict = JSONParser().parse(stream)
         serializer = StudentSerializer(stu,data=request.data)
         if serializer.is_valid():
-            #存数据
             serializer.save()
             return Response(serializer.data,status=status.HTTP_201_CREATED)
         return Response({'error':serializer.errors},status=status.HTTP_400_BAD_REQUEST)
