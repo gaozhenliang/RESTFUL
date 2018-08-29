c475584687543219c637269b9b15bf98b88c34c5
gaozhenliang <1336188490@qq.com>
2018-08-29 21:45:27 +0800

    restful使用mixin类进行对数据请求的基本使用

--- restful/App/views/views.py
+++ restful/App/views/views.py
@@ -1,52 +1,62 @@
-from django.contrib.auth.models import User
-from App.models import Student,Grade
+from App.models import Student
 from App.serializers import StudentSerializer
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from django.http import Http404
+from rest_framework import mixins,generics
 
 # class Index(APIView):
 #     pass
 
-class StudentsList(APIView):
-    def get(self,request,format=None):
-        stus = Student.objects.all()
-        serializer = StudentSerializer(stus,many=True)
-        return Response(serializer.data,status=status.HTTP_200_OK)
 
-    def post(self,request,format=None):
-        serializer = StudentSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data,status=status.HTTP_201_CREATED)
-        return Response({'error':serializer.errors},status=status.HTTP_400_BAD_REQUEST)
+#父类中有且只有一个能继承自APIView类 之所以使用generics.GenericAPIView目的就是为了能够让类继承APIView这个类，能够让这个类既是视图也是类;
+class StudentsList(mixins.ListModelMixin,mixins.CreateModelMixin,generics.GenericAPIView):
+    queryset = Student.objects.all()  # 需得定义此属性，详情请见 mixins.ListModelMixin
+    serializer_class = StudentSerializer  # 需的定义此属性，使用哪个类进行序列化对象
+
+    def get(self,request,*args,**kwargs):
+        return self.list(request,*args,**kwargs)
+
+        # stus = Student.objects.all()
+        # serializer = StudentSerializer(stus,many=True)
+        # return Response(serializer.data,status=status.HTTP_200_OK)
+
+    def post(self,request,*args,**kwargs):
+        return self.create(request,*args,**kwargs)
+        # serializer = StudentSerializer(data=request.data)
+        # if serializer.is_valid():
+        #     serializer.save()
+        #     return Response(serializer.data,status=status.HTTP_201_CREATED)
+        # return Response({'error':serializer.errors},status=status.HTTP_400_BAD_REQUEST)
 
 
-class StudentDetail(APIView):
-    def getObject(self,pk):
-        try:
-            return Student.objects.get(pk=pk)
-        except Student.DoesNotExist as e:
-            raise Http404
+class StudentDetail(mixins.RetrieveModelMixin,mixins.UpdateModelMixin,mixins.DestroyModelMixin,generics.GenericAPIView):
+    # def getObject(self,pk):
+    #     try:
+    #         return Student.objects.get(pk=pk)
+    #     except Student.DoesNotExist as e:
+    #         raise Http404
 
-    def get(self,request,pk,format=None):
-        stu = self.getObject(pk)
-        serializer = StudentSerializer(stu)
-        return Response(serializer.data)
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
 
-    def put(self,request,pk,format=None):
-        stu = self.getObject(pk)
-        serializer = StudentSerializer(stu, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data,status=status.HTTP_201_CREATED)
-        return Response({'error':serializer.errors},status=status.HTTP_400_BAD_REQUEST)
+    def get(self,request,*args,**kwargs):
+        return self.retrieve(request,*args,**kwargs)
+        # stu = self.getObject(pk)
+        # serializer = StudentSerializer(stu)
+        # return Response(serializer.data)
 
-    def delete(self,request,pk,format=None):
-        stu = self.getObject(pk)
-        stu.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def put(self,request,*args,**kwargs):
+        return self.update(request,*args,**kwargs)
+        # stu = self.getObject(pk)
+        # serializer = StudentSerializer(stu, data=request.data)
+        # if serializer.is_valid():
+        #     serializer.save()
+        #     return Response(serializer.data,status=status.HTTP_201_CREATED)
+        # return Response({'error':serializer.errors},status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self,request,*args,**kwargs):
+        return self.destroy(request,*args,**kwargs)
+        # stu = self.getObject(pk)
+        # stu.delete()
+        # return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 
