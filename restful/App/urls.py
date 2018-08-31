94955c278852cadfd717ef7257e7fe230ecd991e
gaozhenliang <1336188490@qq.com>
2018-08-31 14:43:49 +0800

    restful API 之ViewSet和Routers

--- restful/App/urls.py
+++ restful/App/urls.py
@@ -1,20 +1,77 @@
-from django.conf.urls import url
-from .views import views
-#格式后缀
-from rest_framework.urlpatterns import format_suffix_patterns
+from django.conf.urls import url,include
+from App.views.views import StudentViewSet,UserViewSet
+# from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'students',StudentViewSet)
+router.register(r'users',UserViewSet)
+
 
 urlpatterns = [
-    #GET /students
-    #POST /students/
-    url(r'^students/$',views.StudentsList.as_view()),  #as_view()的目的是让它作为视图来用
-    #GET /students/id
-    #PUT /students/id
-    #DELETE /students/id
-    url(r'^students/(?P<pk>\d+)/$', views.StudentDetail.as_view()),
-
-
-    url(r'^users/$',views.UserList.as_view()),
-    url(r'^user/(?P<pk>\d+)/$',views.UserDetail.as_view())
+    url(r'^',include(router.urls))
 ]
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+
+# students_list = StudentViewSet.as_view({
+#     'get':'list',
+#     'post':'create'
+# })
+#
+#
+# student_detail = StudentViewSet.as_view({
+#     'get':'retrieve',
+#     'put':'update',
+#     'patch':'partial_update',
+#     'delete':'destroy'
+# })
+#
+# users_list = UserViewSet.as_view({
+#     'get':'list'
+# })
+#
+# user_detail = UserViewSet.as_view({
+#     'get':'retrieve'
+# })
+
+# urlpatterns = format_suffix_patterns([
+#     url(r'^students/$',students_list,name='students_list'),
+#     url(r'^students/(?P<pk>\d+)/$',student_detail,name='student_detail'),
+#
+#     url(r'^users/$', users_list, name='users_list'),
+#     url(r'^users/(?P<pk>\d+)/$', user_detail, name='user_detail')
+#
+# ])
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from django.conf.urls import url
+# from .views import views
+# #格式后缀
+# from rest_framework.urlpatterns import format_suffix_patterns
+#
+# urlpatterns = [
+#     #GET /students
+#     #POST /students/
+#     url(r'^students/$',views.StudentsList.as_view()),  #as_view()的目的是让它作为视图来用
+#     #GET /students/id
+#     #PUT /students/id
+#     #DELETE /students/id
+#     url(r'^students/(?P<pk>\d+)/$', views.StudentDetail.as_view()),
+#
+#
+#     url(r'^users/$',views.UserList.as_view()),
+#     url(r'^user/(?P<pk>\d+)/$',views.UserDetail.as_view())
+# ]
+#
+# urlpatterns = format_suffix_patterns(urlpatterns)
--- restful/App/views/views.py
+++ restful/App/views/views.py
@@ -1,39 +1,51 @@
 from App.models import Student
 from App.serializers import StudentSerializer,UserSerializer
-from rest_framework import generics
 from rest_framework import permissions
+from django.contrib.auth.models import User
+from App.permissions import IsOwnerOrReadOnly
+from rest_framework import viewsets
 
-
-#父类中有且只有一个能继承自APIView类
-class StudentsList(generics.ListCreateAPIView):
-    queryset = Student.objects.all()  # 需得定义此属性，详情请见 mixins.ListModelMixin
-    serializer_class = StudentSerializer  # 需的定义此属性，使用哪个类进行序列化对象
-
-    #此permission的作用是：如果用户登录则执行以下post请求的代码，否则只能查看数据
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-
-    #让用户在通过post请求创建一个新的student时，在保证创建学生的时会把request中的user赋值给该学生的owner字段
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
-
-from App.permissions import IsOwnerOrReadOnly
-class StudentDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
-
-    #只有所有者用户才能删除、修改，其他用户只能访问
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly)
-
-
-from django.contrib.auth.models import User
-class UserList(generics.ListCreateAPIView):
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-class UserDetail(generics.RetrieveDestroyAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+
+
+# #父类中有且只有一个能继承自APIView类
+# class StudentsList(generics.ListCreateAPIView):
+#     queryset = Student.objects.all()  # 需得定义此属性，详情请见 mixins.ListModelMixin
+#     serializer_class = StudentSerializer  # 需的定义此属性，使用哪个类进行序列化对象
+#
+#     #此permission的作用是：如果用户登录则执行以下post请求的代码，否则只能查看数据
+#     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+#
+#     #让用户在通过post请求创建一个新的student时，在保证创建学生的时会把request中的user赋值给该学生的owner字段
+#     def perform_create(self, serializer):
+#         serializer.save(owner=self.request.user)
+#
+#
+# class StudentDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Student.objects.all()
+#     serializer_class = StudentSerializer
+#
+#     #只有所有者用户才能删除、修改，其他用户只能访问
+#     permission_classes = (permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly)
+#
+#
+# class UserList(generics.ListCreateAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+#
+# class UserDetail(generics.RetrieveDestroyAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
 
 
 
